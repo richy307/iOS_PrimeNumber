0942676e55681c2766e63bc50731f9e89bfb409c
Richy <richy@webcomm.com.tw>
2023-07-17 01:14:29 +0800

    new function 質數判斷邏輯

--- iOS_PrimeNumber/ViewController.swift
+++ iOS_PrimeNumber/ViewController.swift
@@ -14,6 +14,37 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    
+    func checkPrime( withNumber testNumber:Int ) -> String {
+        
+        var isPrime: Bool? = true
+        
+        if testNumber <= 0 {
+            isPrime = nil
+        } else if testNumber == 1 {
+            isPrime = false
+        } else {
+            for i in 2..<testNumber {
+                if testNumber % i == 0 {
+                        // inputNumber is not prime
+                    isPrime = false
+                    break
+                }
+            }
+        }
+        
+        if isPrime == true {
+            return "\(testNumber) is prime"
+        } else if isPrime == false {
+            return "\(testNumber) is not prime"
+        } else {
+            return "Please enter a number greater than 0"
+        }
+    }
+    
+    func checkPrime(withNumber number: Int, andCompletionHandler handler: (String)->() ) {
+        let result = checkPrime(withNumber: number)
+        handler(result)
+    }
 }
 
